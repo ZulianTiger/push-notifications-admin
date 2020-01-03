716eb0dd380ec9c9c4f6d196f360f2a162330451
ZulianTiger <ady0606@gmail.com>
2020-01-03 17:04:38 +0100

    Frontend notification stuff

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,12 +1,112 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, TextInput } from 'react-native'
+import firebase from 'react-native-firebase'
 
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fcmToken: '',
+      notificationTitle: '',
+      notificationBody: '',
+    }
+  }
+
+  async componentDidMount() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      firebase
+        .messaging()
+        .getToken()
+        .then(fcmToken => {
+          if (fcmToken) {
+            this.setState({ fcmToken: fcmToken });
+          } else {
+            alert("user doesn't have a device token yet");
+          }
+        });
+    } else {
+      //request permission ------------
+      firebase.messaging().requestPermission()
+        .then(() => {
+          // User has authorised  
+          firebase
+            .messaging()
+            .getToken()
+            .then(fcmToken => {
+              if (fcmToken) {
+                //TokenService.addUser(fcmToken, "Food is ready!", "Hello user, your food is prepared and ready to be taken.");
+                this.setState({ fcmToken: fcmToken });
+              } else {
+                alert("user doesn't have a device token yet");
+              }
+            });
+        })
+        .catch(error => {
+          // User has rejected permissions  
+          console.log('User has rejected push notification permission.');
+        });
+    }
+
+    this.removeNotificationListener = firebase.notifications().onNotification((Notification) => {
+      // Process your notification as required
+
+      const notification = new firebase.notifications.Notification({
+        sound: 'default',
+        show_in_foreground: true,
+      })
+        .setNotificationId(Notification._notificationId)
+        .setTitle(Notification._title)
+        .setBody(Notification._body)
+        .setData(Notification._data);
+
+      const channel = new firebase.notifications.Android.Channel(
+        'channel7',
+        'Channel 7',
+        firebase.notifications.Android.Importance.Max
+      ).setDescription('The channel used to display notifications in foreground.');
+      firebase.notifications().android.createChannel(channel);
+
+      notification
+        .android.setChannelId('channel7')
+        .android.setSmallIcon('ic_launcher')
+        .android.setPriority(firebase.notifications.Android.Priority.High);
+
+      firebase.notifications()
+        .displayNotification(notification)
+        .catch(err => alert(err));
+
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromNotificationListener();
+  }
   render() {
     return (
       <View style={styles.Container}>
+        <Text style={styles.RegularText}>
+          FCM token: {this.state.fcmToken}
+        </Text>
+
+        <TextInput
+          placeholder={"Notification title"}
+          placeholderTextColor={"#fafafa"}
+          style={styles.TextInput}
+          value={this.state.notificationTitle}
+          onChange={(e) => this.setState({ notificationTitle: e.nativeEvent.text })}
+        />
+
+        <TextInput
+          placeholder={"Notification body text"}
+          placeholderTextColor={"#fafafa"}
+          style={styles.TextInput}
+          value={this.state.notificationBody}
+          onChange={(e) => this.setState({ notificationBody: e.nativeEvent.text })}
+        />
+
         <TouchableOpacity
           style={styles.Button}
           onPress={() => {
--- App/Containers/Styles/LaunchScreenStyles.js
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -1,17 +1,17 @@
 import { StyleSheet } from 'react-native'
-import { Metrics, ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
     Container: {
+        paddingTop: 50,
         width: "100%",
         height: "100%",
         backgroundColor: "#2b2d35",
     },
     Button: {
         height: 65,
-        width: "60%",
+        width: "65%",
         marginLeft: "20%",
-        marginTop: 100,
+        marginTop: 50,
         backgroundColor: "#cc0c1c",
         borderRadius: 25,
     },
@@ -24,5 +24,22 @@ export default StyleSheet.create({
         color: "#fafafa",
         fontWeight: "bold",
         letterSpacing: 1.1,
-    }
+    },
+    RegularText: {
+        width: "60%",
+        marginLeft: "20%",
+        textAlign: "center",
+        color: "#fafafa",
+        paddingBottom: 30,
+    },
+    TextInput: {
+        color: "#fafafa",
+        width: "60%",
+        marginLeft: "20%",
+        borderRadius: 15,
+        borderWidth: 2,
+        borderColor: "#fafafa",
+        marginTop: 20,
+        textAlign: "center",
+    },
 })
