217c0a1002a8678739caa2f33e848e11c898192c
ZulianTiger <ady0606@gmail.com>
2020-01-03 15:54:05 +0100

    Send push notification screen frontend done

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import { Images } from '../Themes'
+import { Text, View, TouchableOpacity } from 'react-native'
 
-// Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
-  render () {
+  render() {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-        </ScrollView>
+      <View style={styles.Container}>
+        <TouchableOpacity 
+          style={styles.Button}
+          onPress={() => {
+            alert("Test");
+          }}
+        >
+          <Text style={styles.ButtonText}>
+            Send Notification
+          </Text>
+        </TouchableOpacity>
       </View>
     )
   }
--- App/Containers/Styles/LaunchScreenStyles.js
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -2,17 +2,27 @@ import { StyleSheet } from 'react-native'
 import { Metrics, ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen,
-  container: {
-    paddingBottom: Metrics.baseMargin
-  },
-  logo: {
-    marginTop: Metrics.doubleSection,
-    height: Metrics.images.logo,
-    width: Metrics.images.logo,
-    resizeMode: 'contain'
-  },
-  centered: {
-    alignItems: 'center'
-  }
+    Container: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#2b2d35",
+    },
+    Button: {
+        height: 65,
+        width: "60%",
+        marginLeft: "20%",
+        marginTop: 100,
+        backgroundColor: "#cc0c1c",
+        borderRadius: 25,
+    },
+    ButtonText: {
+        width: "100%",
+        height: "100%",
+        textAlign: "center",
+        textAlignVertical: "center",
+        fontSize: 22,
+        color: "#fafafa",
+        fontWeight: "bold",
+        letterSpacing: 1.1,
+    }
 })
