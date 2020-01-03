3427783dbab183e25e7679120fe73fd2651e6b54
ZulianTiger <ady0606@gmail.com>
2020-01-03 17:11:01 +0100

    Added background messaging configuration and service

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
+import bgMessaging from './App/Services/bgMessaging'
 
-AppRegistry.registerComponent('test1', () => App)
+AppRegistry.registerComponent('test1', () => App);
+
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
